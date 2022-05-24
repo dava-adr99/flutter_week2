e232c262deae7a76b2a07aa3131e265b110ec640
FlutLab.io <>
2022-05-24 11:32:40 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class BelajarAppBar extends StatelessWidget {
                 pinned: true,
                 flexibleSpace: FlexibleSpaceBar(
                   centerTitle: true,
-                  title: Text("Belajar SliverAppBar",
+                  title: Text("Sprotify",
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 20.0,
