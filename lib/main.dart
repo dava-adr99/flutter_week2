e237890724331384deeede0d433336b9213e2fb0
FlutLab.io <>
2022-05-25 04:40:11 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
 
 class DescribeProduct extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return Scaffold(
       body: Container(
         child Column(
           children: [
