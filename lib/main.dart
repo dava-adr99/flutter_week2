47f733fc182536dec0ab4b4ef759247b7055a293
FlutLab.io <>
2022-05-25 05:02:51 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -82,6 +82,7 @@ class DescribeProduct extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
+                    subtitle: Text('Produk Kami').
                   ),
                 ],
               ),
