2bdb341eb265bb96749c9875610825f8a176b576
FlutLab.io <>
2022-05-25 04:10:11 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class BelajarAppBar extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Container(
-                      width: 100,
+                      height: maxWidth,
                       decoration: BoxDecoration(
                         image: DecorationImage(
                           image: images[index],
