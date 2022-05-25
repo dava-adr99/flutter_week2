58fb1c5b15ab09f3716f5b0e29d132404a9f25a6
FlutLab.io <>
2022-05-25 04:16:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class BelajarAppBar extends StatelessWidget {
         crossAxisCount: 2,
         children: List.generate(6, (index) {
           return Container(
-            height: 300,
+            height: 500,
             margin: EdgeInsets.all(5.0),
             child: Column(children: [
               Image(
